685ee38b4f1e591dc5f65acd852dbc0718482c41
Thomas Seleiro <thomas.seleiro@gmail.com>
2023-03-24 16:06:11 +0000

    Add rust merge sort

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -37,6 +37,37 @@ fn selection_sort(arr: &mut Vec<i32>) -> Vec<i32> {
     return result;
 }
 
+fn merge_sort(arr: &[i32]) -> Vec<i32> {
+    if arr.len() < 2 {
+        return arr.to_vec();
+    } else {
+        let mut result = Vec::with_capacity(arr.len());
+
+        let left = &arr[..arr.len() / 2];
+        let right = &arr[arr.len() / 2..];
+
+        let mut sorted_left = merge_sort(left);
+        sorted_left.reverse();
+        let mut sorted_right = merge_sort(right);
+        sorted_right.reverse();
+
+        while sorted_left.len() > 0 || sorted_right.len() > 0 {
+            let left_value = sorted_left.last().unwrap_or(&i32::MAX);
+            let right_value = sorted_right.last().unwrap_or(&i32::MAX);
+
+            if left_value <= right_value {
+                result.push(*left_value);
+                sorted_left.pop();
+            } else {
+                result.push(*right_value);
+                sorted_right.pop();
+            }
+        }
+
+        return result;
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
@@ -51,5 +82,12 @@ mod tests {
         let mut arr = vec![24, 1, 525, 27, 75];
         let result = vec![1, 24, 27, 75, 525];
         assert_eq!(crate::selection_sort(&mut arr), result);
+    }
+
+    #[test]
+    fn merge_sort() {
+        let arr = vec![24, 1, 525, 27, 75];
+        let result = vec![1, 24, 27, 75, 525];
+        assert_eq!(crate::merge_sort(&arr), result);
     }
 }
