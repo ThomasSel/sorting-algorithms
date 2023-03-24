3cac51aaedf4473e0785023fe7664d4ceaae3b9e
Thomas Seleiro <thomas.seleiro@gmail.com>
2023-03-24 12:08:34 +0000

    Add rust insertion_sort

---
+++ rust/src/lib.rs
@@ -0,0 +1,27 @@
+fn insertion_sort(arr: &Vec<i32>) -> Vec<i32> {
+    let mut result = Vec::with_capacity(arr.len());
+
+    for (number_index, number) in arr.iter().enumerate() {
+        for (result_index, result_number) in result.iter().enumerate() {
+            if number <= result_number {
+                result.insert(result_index, *number);
+                break;
+            }
+        }
+        if result.len() == number_index {
+            result.push(*number);
+        }
+    }
+
+    return result;
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn insertion_sort() {
+        let arr = vec![24, 1, 525, 27, 75];
+        let result = vec![1, 24, 27, 75, 525];
+        assert_eq!(crate::insertion_sort(&arr), result);
+    }
+}
