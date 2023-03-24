6296f38931cdefe300c932bc0fcb81b518ed16aa
Thomas Seleiro <thomas.seleiro@gmail.com>
2023-03-24 14:14:10 +0000

    Add rust selection sort

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -16,6 +16,27 @@ fn insertion_sort(arr: &Vec<i32>) -> Vec<i32> {
     return result;
 }
 
+fn selection_sort(arr: &mut Vec<i32>) -> Vec<i32> {
+    let mut result = Vec::with_capacity(arr.len());
+
+    while arr.len() > 0 {
+        let mut min_number = arr[0];
+        let mut min_index: usize = 0;
+
+        for (number_index, number) in arr.iter().enumerate() {
+            if *number <= min_number {
+                min_number = *number;
+                min_index = number_index;
+            }
+        }
+
+        arr.swap_remove(min_index);
+        result.push(min_number);
+    }
+
+    return result;
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
@@ -23,5 +44,12 @@ mod tests {
         let arr = vec![24, 1, 525, 27, 75];
         let result = vec![1, 24, 27, 75, 525];
         assert_eq!(crate::insertion_sort(&arr), result);
+    }
+
+    #[test]
+    fn selection_sort() {
+        let mut arr = vec![24, 1, 525, 27, 75];
+        let result = vec![1, 24, 27, 75, 525];
+        assert_eq!(crate::selection_sort(&mut arr), result);
     }
 }
