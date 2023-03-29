2b1b67109113fb0dc49a987b599f0f66b041b470
Thomas Seleiro <thomas.seleiro@gmail.com>
2023-03-29 19:17:26 +0100

    Add random large array tests for rust functions

--- rust/src/merge_sort.rs
+++ rust/src/merge_sort.rs
@@ -31,11 +31,36 @@ pub fn copy(arr: &[i32]) -> Vec<i32> {
 
 #[cfg(test)]
 mod tests {
+    use rand::distributions::{Distribution, Uniform};
+
+    fn check_sorted(arr: Vec<i32>) -> bool {
+        for i in 0..(arr.len() - 1) {
+            if arr[i] > arr[i + 1] {
+                return false;
+            }
+        }
+        return true;
+    }
 
     #[test]
-    fn merge_sort() {
+    fn small_array() {
         let arr = vec![24, 1, 525, 27, 75];
         let result = vec![1, 24, 27, 75, 525];
         assert_eq!(crate::merge_sort::copy(&arr), result);
+    }
+
+    #[test]
+    fn large_random_array() {
+        let n = 1000;
+
+        let mut arr: Vec<i32> = Vec::with_capacity(n);
+
+        let mut rng = rand::thread_rng();
+        let distrib: Uniform<i32> = Uniform::from(0..n.try_into().unwrap());
+        for _ in 0..n {
+            arr.push(distrib.sample(&mut rng));
+        }
+
+        assert!(check_sorted(crate::merge_sort::copy(&arr)));
     }
 }
